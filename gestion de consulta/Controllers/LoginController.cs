6507de70bb865a125e0c64d6959022edea5654d9
jhon Jairo Zabala <jhonnyzb1@hotmail.com>
2018-10-01 22:19:50 -0500

    Agregar archivos de proyecto.

---
+++ gestion de consulta/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gestion_de_consulta.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        public ActionResult Registrarse()
+        {
+            return View();
+        }
+
+        public ActionResult Recuperarcontraseña()
+        {
+            return View();
+        }
+    }
+}
